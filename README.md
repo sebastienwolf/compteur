cc3d6c12f3c93a56d9329abdd86fc733c8ec6554
Sébastien Mouret <s.mouret.pro@gmail.com>
2022-01-26 16:49:25 +0100

    first

---
+++ README.md
@@ -0,0 +1,3 @@
+# compteur
+
+Le but de cet exercice est de créer un compteur quand on clique sur l'image.
---
+++ script.js
@@ -0,0 +1,6 @@
+let i = 0
+
+document.getElementById("chocolat").addEventListener("click", function(){
+   i += 1;
+    document.getElementById("compteur").textContent = i;
+})
